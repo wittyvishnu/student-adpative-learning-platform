35173c681577d6c6aa48632c6b01a28a6cb07094
wittyvishnu <vishnuwitty@gmail.com>
2025-05-10 21:19:17 +0530

    final version

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -2,11 +2,8 @@
 
 // Inspired by react-hot-toast library
 import * as React from "react"
+import { ToastActionElement, ToastProps } from "../components/ui/toast"; // Relative path if alias is not set
 
-import type {
-  ToastActionElement,
-  ToastProps,
-} from "@/components/ui/toast"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
